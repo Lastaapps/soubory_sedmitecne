63a44aacd53c3019117deb12b686b41187c59eeb
Petr Laštovička <krasik.peta@seznam.cz>
2024-10-22 12:35:43 +0300

    refactor: Clang 19

--- users/petr/modules/languages/c_cpp/c_cpp.nix
+++ users/petr/modules/languages/c_cpp/c_cpp.nix
@@ -3,12 +3,12 @@
 {
   home.packages = with pkgs; [
     # compiling
-    clang
+    clang_19
     (hiPrio gcc) # high priority used because both clang and gcc share the /bin/c++ binary
     lld
 
     # tools, lsp
-    clang-tools
+    clang-tools_19
 
     # building
     cmake
