61d0b4f67c54c5196b118bcc766fc35401551d89
Petr Laštovička <krasik.peta@seznam.cz>
2024-11-07 15:13:50 +0200

    fix: Safeeyes config conflict

--- users/petr/modules/safeeyes/safeeyes.nix
+++ users/petr/modules/safeeyes/safeeyes.nix
@@ -1,11 +1,17 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   services.safeeyes = {
     enable = true;
   };
 
-  home.file = {
-    "${config.xdg.configHome}/safeeyes/safeeyes.json".source = ./safeeyes.json;
+  # home.file = {
+  #   "${config.xdg.configHome}/safeeyes/safeeyes.json".source = ./safeeyes.json;
+  # };
+  home.activation = {
+    lnSafeeyes = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      run unlink $VERBOSE_ARG ${config.xdg.configHome}/safeeyes/safeeyes.json || true
+      run ln -s $VERBOSE_ARG ~/dotfiles/users/petr/modules/safeeyes/safeeyes.json ${config.xdg.configHome}/safeeyes || true
+    '';
   };
 }
