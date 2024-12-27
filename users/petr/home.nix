ba718c4ed9379520469a5822e75a4a6f65ee2a77
Petr Laštovička <krasik.peta@seznam.cz>
2024-12-28 00:46:28 +0200

    feat: Added Signal desktop

--- users/petr/home.nix
+++ users/petr/home.nix
@@ -59,6 +59,7 @@
     element-desktop
     whatsapp-for-linux
     discord
+    signal-desktop
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
