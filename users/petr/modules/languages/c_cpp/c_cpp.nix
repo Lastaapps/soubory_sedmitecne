2c4627ff5efe5c106f8e27aab7564c454dc6cd7a
Petr Laštovička <krasik.peta@seznam.cz>
2025-02-11 23:00:21 +0200

    feat: Added more C, C++ doc packages

--- users/petr/modules/languages/c_cpp/c_cpp.nix
+++ users/petr/modules/languages/c_cpp/c_cpp.nix
@@ -1,22 +1,25 @@
 { pkgs, ... }:
 
 {
-  home.packages = 
-  (with pkgs.llvmPackages_19; [
-    clang
-    clang-tools
-  ]) ++
-  (with pkgs; [
-    (hiPrio gcc) # high priority used because both clang and gcc share the /bin/c++ binary
-    lld
+  home.packages =
+    (with pkgs.llvmPackages_19; [
+      clang
+      clang-tools
+    ])
+    ++ (with pkgs; [
+      (hiPrio gcc) # high priority used because both clang and gcc share the /bin/c++ binary
+      lld
 
-    # building
-    cmake
-    ninja
-    gnumake
+      # building
+      cmake
+      ninja
+      gnumake
 
-    # debugging
-    gdb
-    valgrind
-  ]);
+      # debugging
+      gdb
+      valgrind
+
+      stdman
+      cppreference-doc
+    ]);
 }
