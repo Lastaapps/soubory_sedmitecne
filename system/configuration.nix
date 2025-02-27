266e56caa74534fdad61495dc8afeeb55ad698b8
Petr Laštovička <krasik.peta@seznam.cz>
2025-02-27 18:01:22 +0200

    feat: Added printer drivers

--- system/configuration.nix
+++ system/configuration.nix
@@ -36,7 +36,25 @@
   };
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      gutenprint
+      gutenprintBin
+      hplip
+      samsung-unified-linux-driver
+      splix
+      brlaser
+      brgenml1lpr
+      cnijfilter2
+    ];
+  };
+  # Printer network discovery
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
