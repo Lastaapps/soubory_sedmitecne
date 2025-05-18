410373bde80dea9aa9b06d61b6149b2a48411a6f
Petr Laštovička <krasik.peta@seznam.cz>
2025-05-18 15:57:12 +0300

    feat: Update script shows differing packages

--- scripts/update_and_reapply_all.sh
+++ scripts/update_and_reapply_all.sh
@@ -8,5 +8,11 @@ pushd ~/dotfiles/scripts > /dev/null || exit
 ./apply_system.sh
 ./apply_home.sh
 
+echo "--------------------------------------------------------------------------------"
+nix profile diff-closures --profile /nix/var/nix/profiles/system
+echo "--------------------------------------------------------------------------------"
+nix profile diff-closures --profile ~/.local/state/nix/profiles/home-manager
+echo "--------------------------------------------------------------------------------"
+
 popd > /dev/null || exit
 
