0d662de3eb25e911cfc4cee482b722129ed9ca06
Petr Laštovička <krasik.peta@seznam.cz>
2025-07-02 19:32:27 +0300

    feat: Add safeeyes handler outside of gnome

--- users/petr/modules/gui/safeeyes/default.nix
+++ users/petr/modules/gui/safeeyes/default.nix
@@ -7,6 +7,14 @@ in
   services.safeeyes = {
     enable = true;
   };
+  services.snixembed = {
+    enable = true;
+
+    beforeUnits = [
+      # https://github.com/slgobinath/SafeEyes/wiki/How-to-install-backend-for-Safe-Eyes-tray-icon
+      "safeeyes.service"
+    ];
+  };
 
   xdg.configFile."safeeyes/safeeyes.json".source =
     config.lib.file.mkOutOfStoreSymlink safeEyesConfigPath;
