bde12fd1cc36bab89b6e0d11c8a08dd9a2e9cf95
Petr Laštovička <krasik.peta@seznam.cz>
2025-07-04 15:07:40 +0300

    feat: Add update service

--- system/configuration.nix
+++ system/configuration.nix
@@ -26,6 +26,8 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  services.fwupd.enable = true;
+
   # Configure keymap in X11
   services.xserver.xkb = {
     # layout = "us";
