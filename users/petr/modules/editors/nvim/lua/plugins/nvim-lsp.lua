e22cac5dd05a15c22b9972e0b7e340c023236cc6
Petr Laštovička <krasik.peta@seznam.cz>
2025-12-03 16:13:53 +0200

    refactor(nvim): Migrated lsp to NVim 0.11

--- users/petr/modules/editors/nvim/lua/plugins/nvim-lsp.lua
+++ users/petr/modules/editors/nvim/lua/plugins/nvim-lsp.lua
@@ -1,344 +1,157 @@
--- Configure communication with LSP servers
-
--- Set configuration for specific filetype.
-local configureLanguageServers = function()
-    local lspconfig = require('lspconfig')
-
-    -- Set global defaults for all servers
-    lspconfig.util.default_config = vim.tbl_extend(
-        'force',
-        lspconfig.util.default_config,
-        {
-            capabilities = vim.tbl_deep_extend(
-                "force",
-                vim.lsp.protocol.make_client_capabilities(),
-                require('lsp-file-operations').default_capabilities()
-            )
-        }
-    )
-
-    -- Connection to LSP hrsh7th/cmp-nvim-lsp
-    local capabilities = require('cmp_nvim_lsp').default_capabilities(
-    -- vim.lsp.protocol.make_client_capabilities()
-    )
-
-    -- C/C++
-    lspconfig.clangd.setup {
-        capabilities = capabilities,
-        root_dir = lspconfig.util.root_pattern(
-            '.clangd',
-            '.clang-tidy',
-            '.clang-format',
-            'compile_commands.json',
-            'compile_flags.txt',
-            'configure.ac',
-            'Makefile',
-            '.git'
-        ),
-    }
-
-    -- Kotlin
-    lspconfig.kotlin_language_server.setup {
-        capabilities = capabilities,
-        root_dir = lspconfig.util.root_pattern('settings.gradle.kts', 'settings.gradle', '.git', '*'),
-    }
-
-    -- Python
-    local use_pyright = true
-    if use_pyright then
-        lspconfig.pyright.setup {
-            capabilities = capabilities,
-            root_dir = lspconfig.util.root_pattern('.git', '.venv'),
-        }
-    else
-        lspconfig.pylsp.setup {
-            capabilities = capabilities,
-            -- settings = {
-            --   pylsp = {
-            --     plugins = {
-            --       pycodestyle = {
-            --         ignore = {'W391'},
-            --         maxLineLength = 100
-            --       }
-            --     }
-            --   }
-            -- }
-        }
-    end
-
-
-    -- Lua
-    lspconfig.lua_ls.setup {
-        capabilities = capabilities,
-        settings = {
-            Lua = {
-                runtime = {
-                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                    version = 'LuaJIT',
-                },
-                diagnostics = {
-                    -- Get the language server to recognize the `vim` global
-                    globals = { 'vim' },
-                },
-                workspace = {
-                    -- Make the server aware of Neovim runtime files
-                    library = vim.api.nvim_get_runtime_file("", true),
-                    -- Remove some weird message
-                    checkThirdParty = false,
-                },
-                -- Do not send telemetry data containing a randomized but unique identifier
-                -- telemetry = {
-                --     enable = false,
-                -- },
-            },
-        },
-    }
-
-    -- Bash
-    lspconfig.bashls.setup {
-        capabilities = capabilities,
-    }
-
-    -- Docker
-    lspconfig.dockerls.setup {
-        capabilities = capabilities,
-    }
-
-    -- LaTex, Tex
-    -- https://github.com/latex-lsp/texlab/wiki/Configuration
-    lspconfig.texlab.setup {
-        capabilities = capabilities,
-        before_init = function(params)
-            params.processId = vim.NIL
-        end,
-        settings = {
-            texlab = {
-                build = {
-                    args = {
-                        "-pdf",
-                        "-interaction=nonstopmode",
-                        "-synctex=1",
-                        "-auxdir=build",
-                        "%f",
-                    },
-                    auxDirectory = "build",
-                    logDirectory = "build",
-                    onSave = true,
-                    useFileList = true,
-                    forwardSearchAfter = true,
-                },
-                chktex = {
-                    onEdit = true,
-                    onOpenAndSave = true,
-                },
-                latexindent = {
-                    args = { '-l' },
-                },
-                experimental = {
-                    followPackageLinks = true,
-                },
-            }
-        }
-    }
-
-    -- YAML
-    lspconfig.yamlls.setup {
-        capabilities = capabilities,
-        settings = {
-            yaml = {
-                schemas = {
-                    ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-                    -- ["../path/relative/to/file.yml"] = "/.github/workflows/*",
-                    -- ["/path/from/root/of/project"] = "/.github/workflows/*",
-                    ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
-                },
-            },
-        }
-    }
-
-    -- Verilog
-    lspconfig.svls.setup {
-        capabilities = capabilities,
-        -- root_dir = lspconfig.util.root_pattern('.git', '*'),
-    }
-
-    -- Racket
-    lspconfig.racket_langserver.setup {
-        capabilities = capabilities,
-    }
-
-    -- Rust
-    lspconfig.rust_analyzer.setup {
-        capabilities = capabilities,
-    }
-
-    -- English - the worst of them all
-    local ltex_vim_filetypes = {
-        -- ltex server uses VSCode like file types that do not always match with NVim
-        -- ltex-plus NVim integration maps the most annoying of them (the default ones),
-        -- still note that if an added language does not work,
-        -- check the VSCode list and add both the names
-        -- https://code.visualstudio.com/docs/languages/identifiers
-        -- Non-supported languages are checked as plain files,
-        -- remove them from this list if found
-        "bash",
-        "c", "cpp",
-        "go",
-        "haskell",
-        "json",
-        "lua",
-        "python",
-        "rust",
-        "typst",
-        "yaml",
-        "zip",
-        -- defaults
-        "bib", "gitcommit", "markdown", "org", "plaintex", "rst", "rnoweb", "tex", "pandoc", "quarto", "rmd", "context",
-        "html", "xhtml", "mail", "text",
-    }
-    lspconfig.ltex_plus.setup {
-        capabilities = capabilities,
-        flags = { debounce_text_changes = 300 },
-        on_attach = function(client, bufnr)
-            require("ltex_extra").setup {
-                load_langs = { "en-US" },
-                log_lever = "error",
-                path = "ltex",
-            }
-        end,
-        filetypes = ltex_vim_filetypes,
-        settings = {
-            -- https://valentjn.github.io/ltex/settings.html
-            ltex = {
-                -- https://valentjn.github.io/ltex/settings.html
-                language = "en-US",
-                completionEnabled = true,
-                enabled = ltex_vim_filetypes,
-                additional_rules = {
-                    enablePickyRules = true,
-                    -- languageModel = "~/language_tool_models/ngrams",
-                },
-                -- Easily causes to many requests
-                -- languageToolHttpServerUri = "http://localhost:8081/",
-                -- languageToolHttpServerUri = "https://api.languagetool.org/",
-                -- languageToolOrg = {
-                --     username = "",
-                --     apiKey = "",
-                -- },
-
-                -- possibly not working
-                ltex_ls = {
-                    -- logLevel = "severe",
-                    logLevel = "config",
-                    -- logLevel = "finest",
-                },
-            },
-        },
-    }
-    -- Vale Linter for English
-    lspconfig.vale_ls.setup {
-        capabilities = capabilities,
-        -- filetypes = { "markdown", "text", "plain", "tex", "rst" },
-        filetypes = ltex_vim_filetypes,
-        single_file_support = false, -- runs only if a config file is found
-    }
-
-    -- Typst
-    lspconfig.tinymist.setup {
-        capabilities = capabilities,
-        root_dir = lspconfig.util.root_pattern('.git', '*'),
-    }
-
-    -- GoLang
-    lspconfig.gopls.setup {
-        capabilities = capabilities,
-    }
-
-    -- Zig (zls)
-    lspconfig.zls.setup {
-        capabilities = capabilities,
-    }
-
-    -- GLSL - OpenGL
-    lspconfig.glsl_analyzer.setup {
-        capabilities = capabilities,
-    }
-
-    -- CMake
-    -- lspconfig.cmake.setup {
-    --     capabilities = capabilities,
-    -- }
-    lspconfig.neocmake.setup {
-        capabilities = capabilities,
-    }
-
-    -- SQL
-    lspconfig.sqls.setup {
-        capabilities = capabilities,
-        on_attach = function(client, bufnr)
-            require('sqls').on_attach(client, bufnr)
-        end
-    }
-
-    -- Nix
-    lspconfig.nixd.setup {
-        capabilities = capabilities,
-    }
-    lspconfig.nil_ls.setup {
-        capabilities = capabilities,
-        settings = {
-            ['nil'] = {
-                testSetting = 42,
-                formatting = {
-                    command = { "nixfmt" },
-                },
-                nix = {
-                    flake = {
-                        -- Automatically run `nix flake archive` when necessary.
-                        autoArchive = true,
-                        -- Whether to auto-eval flake inputs.
-                        -- The evaluation result is used to improve completion, but may cost
-                        -- lots of time and/or memory.
-                        -- autoEvalInputs = true,
-                    }
-                },
-            },
-        },
-    }
-
-    -- Haskell
-    -- set using the haskel-tools
-    lspconfig.hls.setup {
-        capabilities = capabilities,
-        filetypes = { 'haskell', 'lhaskell', 'cabal' },
-    }
-
-    -- Elm
-    lspconfig.elmls.setup {
-        capabilities = capabilities,
-    }
-end
-
 return {
     {
         'neovim/nvim-lspconfig',
-        event = "BufReadPost",
+        event = "VeryLazy",
         dependencies = {
             "antosha417/nvim-lsp-file-operations",
+            "hrsh7th/cmp-nvim-lsp",
         },
         config = function()
-            configureLanguageServers()
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                vim.lsp.protocol.make_client_capabilities(),
+                require('lsp-file-operations').default_capabilities(),
+                require('cmp_nvim_lsp').default_capabilities()
+            )
+
+            local ltex_filetypes = {
+                "bash", "c", "cpp", "go", "haskell", "json", "lua", "python", "rust", "typst", "yaml", "zip",
+                "bib", "gitcommit", "markdown", "org", "plaintex", "rst", "rnoweb", "tex", "pandoc", "quarto",
+                "rmd", "context", "html", "xhtml", "mail", "text",
+            }
+
+            local servers = {
+                clangd = {
+                    root_markers = { '.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', 'configure.ac', 'Makefile', '.git' },
+                },
+                kotlin_language_server = {
+                    root_markers = { 'settings.gradle.kts', 'settings.gradle', '.git' },
+                },
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            runtime = { version = 'LuaJIT' },
+                            diagnostics = { globals = { 'vim' } },
+                            workspace = {
+                                library = vim.api.nvim_get_runtime_file("", true),
+                                checkThirdParty = false,
+                            },
+                        },
+                    },
+                },
+                bashls = {},
+                dockerls = {},
+                texlab = {
+                    settings = {
+                        texlab = {
+                            build = {
+                                args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "-auxdir=build", "%f" },
+                                auxDirectory = "build",
+                                logDirectory = "build",
+                                onSave = true,
+                                useFileList = true,
+                                forwardSearchAfter = true,
+                            },
+                            chktex = { onEdit = true, onOpenAndSave = true },
+                            latexindent = { args = { '-l' } },
+                            experimental = { followPackageLinks = true },
+                        }
+                    }
+                },
+                yamlls = {
+                    settings = {
+                        yaml = {
+                            schemas = {
+                                ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+                                ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] =
+                                "/*.k8s.yaml",
+                            },
+                        }
+                    }
+                },
+                svls = {},
+                racket_langserver = {},
+                rust_analyzer = {},
+                ltex_plus = {
+                    filetypes = ltex_filetypes,
+                    flags = { debounce_text_changes = 300 },
+                    on_attach = function()
+                        require("ltex_extra").setup {
+                            load_langs = { "en-US" },
+                            log_lever = "error",
+                            path = "ltex",
+                        }
+                    end,
+                    settings = {
+                        ltex = {
+                            language = "en-US",
+                            completionEnabled = true,
+                            enabled = ltex_filetypes,
+                            additional_rules = { enablePickyRules = true },
+                            ltex_ls = { logLevel = "config" },
+                        },
+                    },
+                },
+                vale_ls = {
+                    filetypes = ltex_filetypes,
+                    single_file_support = false,
+                },
+                tinymist = {
+                    root_markers = { '.git' }, -- tinymist usually detects root well, but explicit marker added
+                },
+                gopls = {},
+                zls = {},
+                glsl_analyzer = {},
+                neocmake = {},
+                sqls = {
+                    on_attach = function(client, bufnr)
+                        require('sqls').on_attach(client, bufnr)
+                    end
+                },
+                nixd = {},
+                nil_ls = {
+                    settings = {
+                        ['nil'] = {
+                            testSetting = 42,
+                            formatting = { command = { "nixfmt" } },
+                            nix = { flake = { autoArchive = true } },
+                        },
+                    },
+                },
+                hls = {
+                    filetypes = { 'haskell', 'lhaskell', 'cabal' },
+                },
+                elmls = {},
+            }
+
+            -- 4. Logic Branches
+            -- Python selection
+            local use_pyright = true
+            if use_pyright then
+                servers.pyright = {
+                    root_markers = { '.git', '.venv', 'pyproject.toml' },
+                }
+            else
+                servers.pylsp = {}
+            end
+
+            -- 5. Application Loop
+            -- Apply configuration and enable
+            for server, config in pairs(servers) do
+                -- Inject standard capabilities into every config
+                config.capabilities = vim.tbl_deep_extend("force", capabilities, config.capabilities or {})
+
+                -- Register the config (merges with nvim-lspconfig's defaults automatically)
+                vim.lsp.config[server] = config
+
+                -- Activate the server (sets up FileType autocommands)
+                vim.lsp.enable(server)
+            end
         end
     },
-    -- Support for moving/renaming files while using nvim-tree, neo-tree or triptych
     {
         "antosha417/nvim-lsp-file-operations",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            -- "nvim-tree/nvim-tree.lua",
-            -- "nvim-neo-tree/neo-tree.nvim",
-            -- "simonmclean/triptych.nvim"
-        },
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             require("lsp-file-operations").setup()
         end,
