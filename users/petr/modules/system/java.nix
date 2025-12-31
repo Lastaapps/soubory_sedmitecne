dfd9b1956870220907367f01e072a5b678cf5dd6
Petr Laštovička <krasik.peta@seznam.cz>
2025-12-31 15:57:09 +0200

    feat: Add Java 17

--- users/petr/modules/system/java.nix
+++ users/petr/modules/system/java.nix
@@ -5,4 +5,10 @@
     enable = true;
     package = pkgs.jdk21;
   };
+
+  home.sessionVariables = {
+    JAVA_17_HOME = "${pkgs.jdk17}/lib/openjdk";
+    JAVA_21_HOME = "${pkgs.jdk21}/lib/openjdk";
+  };
+
 }
