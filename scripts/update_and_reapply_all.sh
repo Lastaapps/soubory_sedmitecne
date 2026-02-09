4bfa4d3101d55fae80b110518c2be3ae545543b1
Petr Laštovička <krasik.peta@seznam.cz>
2026-02-10 00:26:01 +0200

    refactor: Separate latest changes showing logic

--- scripts/update_and_reapply_all.sh
+++ scripts/update_and_reapply_all.sh
@@ -7,27 +7,7 @@ pushd ~/dotfiles/scripts > /dev/null || exit
 ./update_flake.sh
 ./apply_system.sh
 ./apply_home.sh
-
-nix_last_diff() {
-    local profile_base_path="$1"
-    local cmd_prefix=()
-
-    # Add 'sudo' to cmd_prefix if the profile is the system profile.
-    [[ "$profile_base_path" == "/nix/var/nix/profiles/system"* ]] && cmd_prefix=(sudo)
-
-    # Get the two most recent generation links
-    local gens=$("${cmd_prefix[@]}" ls -t1d "${profile_base_path}"-*-link 2>/dev/null | head -2)
-    local old_gen=$(echo "$gens" | sed -n '2p')
-    local new_gen=$(echo "$gens" | head -n 1)
-
-    "${cmd_prefix[@]}" nix store diff-closures "$old_gen" "$new_gen"
-}
-
-echo "--------------------------------------------------------------------------------"
-nix_last_diff /nix/var/nix/profiles/system
-echo "--------------------------------------------------------------------------------"
-nix_last_diff ~/.local/state/nix/profiles/home-manager
-echo "--------------------------------------------------------------------------------"
+./latest_changes.sh
 
 popd > /dev/null || exit
 
