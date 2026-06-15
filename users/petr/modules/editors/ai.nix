271d75c2841a83e51224a5a242e7f1e761bd6412
Petr Laštovička <krasik.peta@seznam.cz>
2026-06-16 01:30:51 +0300

    refactor(ai): Switch to Antigravity-cli

--- users/petr/modules/editors/ai.nix
+++ users/petr/modules/editors/ai.nix
@@ -1,7 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 
 {
+  programs.antigravity-cli = {
+    enable = true;
+    enableMcpIntegration = true;
+    package = pkgs-unstable.antigravity-cli;
+  };
+
   home.packages = with pkgs; [
-    gemini-cli-bin
   ];
 }
